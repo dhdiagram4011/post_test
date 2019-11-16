66cc19449bed3553b40b4c57401581557c4ca5f7
dhdiagram@gmail.com <dhdiagram@gmail.com>
2019-11-16 12:44:38 +0900

    [update]Code Upate - Airline Ticketing System

--- tcode/testapp/urls.py
+++ tcode/testapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name='testapp'
 urlpatterns  = [
     path('',views.join, name='join'),
     path('',views.join_result, name='join_result'),
     path('',views.translate, name='translate'),
-    path('',views.translate_result, name='translate_result')
+    path('',views.translate_result, name='translate_result'),
     path('',views.selecttest, name='selecttest'),
     path('',views.selecttest_result, name='selecttest_result'),
     path('',views.main, name='main'),
@@ -13,5 +14,6 @@ urlpatterns  = [
     path('',views.login, name='login'),
     path('',views.login_success, name='login_success'),
     path('',views.logout, name='logout'),
+    path('',views.serverlist, name='serverlist'),
 ]
 
